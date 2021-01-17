d70bd481ebd8f87a977d790be71f02497db48a78
kwabounga <jeanyves.chaillou@gmail.com>
2021-01-17 15:28:02 +0100

    locales update : force fr_FR / en_US

--- components/texts.js
+++ components/texts.js
@@ -30,7 +30,7 @@ const locales = {
             made:"Made!"
         },
         swipeables:{
-            archivate:"Archivate",
+            archivate:"Archive",
             reactivate:"Reactivate",
             delete:"Delete",
         }
@@ -38,5 +38,38 @@ const locales = {
 }
 
 export const local = () => {
-    return  locales[deviceLanguage];
+    let language  = '';
+    switch (deviceLanguage) { 
+        case 'fr_BE':
+        case 'fr_BE@euro':
+        case 'fr_CA':
+        case 'fr_CH':
+        case 'fr_FR':
+        case 'fr_FR@euro':
+        case 'fr_LU':
+        case 'fr_LU@euro':   
+            language = 'fr_FR';
+            break;
+        case 'en_AG':
+        case 'en_AU':
+        case 'en_BW':
+        case 'en_CA':
+        case 'en_DK':
+        case 'en_GB':
+        case 'en_HK':
+        case 'en_IE':
+        case 'en_IN':
+        case 'en_NG':
+        case 'en_NZ':
+        case 'en_PH':
+        case 'en_SG':
+        case 'en_US':
+        case 'en_ZA':
+        case 'en_ZM':
+        case 'en_ZW':
+        default:
+            language = 'en_US';
+            break;
+    }
+    return  locales[language];
 }
